a3330b56f60ae1419b90289ab0731e8df7aec1df
KeyichenFirst <93365266+KeyichenFirst@users.noreply.github.com>
2024-10-08 11:58:34 +0800

    10.8

--- miniob-main/src/observer/sql/executor/drop_table_executor.cpp
+++ miniob-main/src/observer/sql/executor/drop_table_executor.cpp
@@ -4,7 +4,7 @@
 #include "event/session_event.h"
 #include "event/sql_event.h"
 #include "session/session.h"
-#include "sql/stmt/create_table_stmt.h"
+#include "sql/stmt/drop_table_stmt.h"
 #include "storage/db/db.h"
 
 RC DropTableExecutor::execute(SQLStageEvent *sql_event)
@@ -15,7 +15,7 @@ RC DropTableExecutor::execute(SQLStageEvent *sql_event)
       "drop table executor can not run this command: %d",
       static_cast<int>(stmt->type()));
 
-  DropTableStmt *create_table_stmt = static_cast<DropTableStmt *>(stmt);
+  DropTableStmt *drop_table_stmt = static_cast<DropTableStmt *>(stmt);
 
   const char *table_name = drop_table_stmt->table_name().c_str();
   RC rc = session->get_current_db()->drop_table(table_name);
